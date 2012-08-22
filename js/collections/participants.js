a566081ac7c0c403c525dcfea60b1086fde14918
bmeurant <baptiste.meurant@gmail.com>
2012-08-22 23:51:08 +0200

    change collection fetch url to match with new version of tournament-server-java

--- js/collections/participants.js
+++ js/collections/participants.js
@@ -20,7 +20,7 @@ define([
             dataType:'json',
 
             // the URL (or base URL) for the service
-            url:App.Config.serverRootURL + '/participant/search'
+            url:App.Config.serverRootURL + '/participant'
         },
         paginator_ui:{
             // the lowest page index your API allows to be accessed
