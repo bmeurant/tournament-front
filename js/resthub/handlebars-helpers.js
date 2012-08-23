662e9d6dccd8dc3d2adfcc32f157f783c2f4d1f3
bmeurant <baptiste.meurant@gmail.com>
2012-08-24 00:05:30 +0200

    backport resthub update of handlebars helpers

--- js/resthub/handlebars-helpers.js
+++ js/resthub/handlebars-helpers.js
@@ -1,58 +1,63 @@
 /**
  * Set of generic handlebars helpers
- *
- * Author: Baptiste Meurant <baptiste.meurant@gmail.com>
- * Date: 23/08/12
- * Time: 12:14
  */
-
-(function (factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory(require('handlebars'));
-    } else if (typeof define === 'function' && define.amd) {
-        define(['handlebars'], factory);
-    }
-}(function (Handlebars) {
-
+define(['handlebars', 'underscore.string'], function (Handlebars, _s) {
     /**
      * This helper provides a more fluent syntax for inline ifs. i.e. if
      * embedded in quoted strings
      *
+     * As with Handlebars 'if', if its first argument returns false, undefined,
+     * null or [] (a "falsy" value), '' is returned, otherwise returnVal
+     * argument is rendered.
+     *
      * Usage: class='{{ifinline done "done"}}'
+     *
      */
     Handlebars.registerHelper('ifinline', function (value, returnVal) {
-        return value ? returnVal : '';
+        return (value && !Handlebars.Utils.isEmpty(value)) ? returnVal : '';
     });
 
     /**
      * Opposite of ifinline helper
      *
+     * As with Handlebars 'unless', if its first argument returns false, undefined,
+     * null or [] (a "falsy" value), '' returnVal argument is returned, otherwise ''
+     * is rendered.
+     *
      * Usage: class='{{unlessinline done "todo"}}'
      */
     Handlebars.registerHelper('unlessinline', function (value, returnVal) {
-        return value ? '' : returnVal;
+        return (value && !Handlebars.Utils.isEmpty(value)) ? '' : returnVal;
     });
 
     /**
-     * This helper provides a if inline comparing two values
+     * This helper provides a if inline comparing two values.
      *
-     * Usage: class='{{ifequalsinline type "details" "active"}}'
+     * If the two values are strictly equals ('===') return the returnValue
+     * argument, '' otherwise.
+     *
+     * Usage: class='{{ifequalsinline id 1 "active"}}'
      */
     Handlebars.registerHelper('ifequalsinline', function (value1, value2, returnVal) {
-        return (value1 == value2) ? returnVal : '';
+        return (value1 === value2) ? returnVal : '';
     });
 
     /**
      * Opposite of ifequalsinline helper
      *
-     * Usage: class='{{unlessequalsinline type "details" "disabled"}}'
+     * If the two values are not strictly equals ('===') return the returnValue
+     * argument, '' otherwise.
+     *
+     * Usage: class='{{unlessequalsinline id 1 "disabled"}}'
      */
     Handlebars.registerHelper('unlessequalsinline', function (value1, value2, returnVal) {
-        return (value1 == value2) ? '' : returnVal;
+        return (value1 === value2) ? '' : returnVal;
     });
 
     /**
      * This helper provides a if comparing two values
+     *
+     * If only the two values are strictly equals ('!==') display the block
      *
      * Usage:
      *        {{#ifequals type "details"}}
@@ -61,7 +66,7 @@
      */
     Handlebars.registerHelper('ifequals', function (value1, value2, options) {
 
-        if (value1 == value2) {
+        if (value1 === value2) {
             return options.fn(this);
         } else {
             return options.inverse(this);
@@ -70,6 +75,8 @@
 
     /**
      * Opposite of ifequals helper
+     *
+     * If only the two values are not strictly equals ('!==') display the block
      *
      * Usage:
      *        {{#unlessequals type "details"}}
@@ -87,6 +94,10 @@
     /**
      * This helper provides a for i in range loop
      *
+     * start and end parameters have to be integers >= 0 or their
+     * string representation. start should be <= end.
+     * In all other cases, not block is rendered.
+     *
      * Usage:
      *        <ul>
      *            {{#for 0 10}}
@@ -96,8 +107,8 @@
      */
     Handlebars.registerHelper('for', function (start, end, options) {
         var fn = options.fn, inverse = options.inverse;
-        var isStartValid = (start && !isNaN(parseInt(start)));
-        var isEndValid = (end && !isNaN(parseInt(end)));
+        var isStartValid = (start != undefined && !isNaN(parseInt(start)) && start >= 0);
+        var isEndValid = (end != undefined && !isNaN(parseInt(end)) && end >= 0);
         var ret = "";
 
         if (isStartValid && isEndValid && parseInt(start) <= parseInt(end)) {
@@ -110,4 +121,14 @@
 
         return ret;
     });
-}));
+
+    /**
+     * sprintf support, useful for internationalization when used with RequireJS i18n extension
+     *
+     * Usage: class='{{sprintf "Welcome %s !" username}}'
+     */
+    Handlebars.registerHelper('sprintf', function () {
+        return _.str.sprintf.apply(this, arguments);
+    });
+
+});
